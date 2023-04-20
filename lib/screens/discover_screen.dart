62ce4cf895c829fdbca62e87115bc9e1c4a48b75
TonnyKamau <tonnykamau6gmail.com>
2023-04-21 00:04:15 +0300

    plugins

--- lib/screens/discover_screen.dart
+++ lib/screens/discover_screen.dart
@@ -1,11 +1,30 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/material.dart';
+
+import '../widgets/widget.dart';
 
 class DiscoverScreen extends StatelessWidget {
   const DiscoverScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
+          leading: IconButton(
+            icon: const Icon(Icons.menu, color: Colors.white, size: 30.0),
+            onPressed: () {},
+          ),
+        ),
+        bottomNavigationBar: const BottomNavBar(
+          selectedIndex: 1,
+        ),
+        extendBodyBehindAppBar: true,
+        body: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+           
+          ],
+        ));
   }
 }
