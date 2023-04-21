da1a0b25d79a7c653e1438c3b187fd4523fd7397
TonnyKamau <tonnykamau6@gmail.com>
2023-04-21 16:52:54 +0300

    new

--- lib/screens/discover_screen.dart
+++ lib/screens/discover_screen.dart
@@ -167,6 +167,7 @@ class _DiscoverNews extends StatelessWidget {
               style: Theme.of(context).textTheme.bodySmall),
           const SizedBox(height: 20.0),
           TextFormField(
+            cursorColor: Colors.black,
             decoration: InputDecoration(
               hintText: 'Search',
               fillColor: Colors.grey.shade200,
